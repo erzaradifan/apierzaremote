f5e422d623ef16654b88576f7bfe5814ae5b79d2
erza <erzaradifan@gmail.com>
2019-10-21 22:38:22 +0700

    commit 7 belajar node

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    bodyParser = require('body-parser'),
-    controller = require('./controller');
+//var express = require('express'),
+//    app = express(),
+//    port = process.env.PORT || 3000,
+//    bodyParser = require('body-parser'),
+//    controller = require('./controller');
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const controller = require('./controller')
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
